c8f66a8ff8e7bf25bc3ce1283f9f9f68f3ed37ed
Andy Ju <andy.ju@duke.edu>
2019-12-09 20:39:27 -0500

    Updates

---
+++ precache-manifest.1f5331d94e6d889a73333b83ccfef656.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9d7c6bf8a6967e90f00c353ab8c8eda",
+    "url": "/justforplay-react/index.html"
+  },
+  {
+    "revision": "3fb08112b451ea75aee7",
+    "url": "/justforplay-react/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "1689ff8bcc849ed487da",
+    "url": "/justforplay-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3fb08112b451ea75aee7",
+    "url": "/justforplay-react/static/js/2.95764734.chunk.js"
+  },
+  {
+    "revision": "1689ff8bcc849ed487da",
+    "url": "/justforplay-react/static/js/main.88b14f4a.chunk.js"
+  },
+  {
+    "revision": "f6e0a1b5a8e6a271a8c5",
+    "url": "/justforplay-react/static/js/runtime-main.7510fb75.js"
+  }
+]);
